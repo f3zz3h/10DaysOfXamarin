8887496520855dc5bc34109682d5e373f46547a4
Luke Hart <luke.m.hart@outlook.com>
2019-10-23 22:29:52 +0100

    First day completed

---
+++ 10DaysOfXamarin-Day1/TenDaysOfXamarin/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TenDaysOfXamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(nameEntry.Text))
+            {
+                greetingLabel.Text = $"Hello {nameEntry.Text}, welcome to 10 Days of Xamarin.";
+            }
+            else
+            {
+                DisplayAlert("Error", "Your name can't be empty", "Oh right");
+            }
+        }
+    }
+}
